a9f398d9a9c2cc5a9d286f5b198ed717c84d6453
Nathan Boyd <nathan.boyd.481@my.csun.edu>
2019-01-01 11:11:23 -0800

    fixed segment fault in trajectory generation

--- src/leg_trajectory_client.cpp
+++ src/leg_trajectory_client.cpp
@@ -19,9 +19,10 @@ class LegParabola
             //spin thread by default
             //defines the namespace and target leg_action_server, where the info will be published
             //topics sent out are along this path
-            traj_client = new Client("leg_"+leg_id+"_traj_controller/joint_trajectory_action_server", true);
+           // traj_client = new Client("leg_rf_traj_controller/joint_trajectory_action_server", true);
+            traj_client = new Client("joint_trajectory_action_server",true);    
             while(!traj_client->waitForServer(ros::Duration(5.0))){
-                ROS_INFO("Waiting for the joint_trajectory_action_server server");
+                ROS_INFO("Waiting for the leg_%s_traj_controller/joint_trajectory_action_server",leg_id.c_str());
             }
         }
 
@@ -36,6 +37,7 @@ class LegParabola
             // Send the trajectory to follow
             goal.trajectory.header.stamp = ros::Time::now();
             traj_client->sendGoal(goal);
+            ROS_INFO("Sent Goal Trajectory");
         } 
 
         /*
@@ -45,32 +47,41 @@ class LegParabola
         {
             ardent_controllers_msgs::JointTrajectoryGoal goal;
 
+            ROS_INFO("Creating trajectory");
             // Joint names that apply to waypoint list
             goal.trajectory.joint_names.push_back("j_femur_"+leg_id);
             goal.trajectory.joint_names.push_back("j_coxa_"+leg_id);
             goal.trajectory.joint_names.push_back("j_tibia_"+leg_id);
             
+            
             // 2 waypoints are in the list
             goal.trajectory.points.resize(2);
-
             // First trajectory waypoint poisition for each joint is 0
             int i =0;
+            goal.trajectory.points[i].positions.resize(3);
             goal.trajectory.points[i].positions[0] = 0.0; 
             goal.trajectory.points[i].positions[1] = 0.0;
             goal.trajectory.points[i].positions[2] = 0.0;
-            for(int j=0;j<goal.trajectory.points.size();i++)
+            goal.trajectory.points[i].velocities.resize(3);
+            goal.trajectory.points[i].accelerations.resize(3);
+            for(int j=0;j<3;++j)
             {
                 goal.trajectory.points[i].velocities[j] = 0.0;
                 goal.trajectory.points[i].accelerations[j] = 0.0;
+
+                ROS_INFO("Added trajectory for %i joint", j);
             }
             // 1st Waypoint goal should be reached 1 second after starting along trajectory
             goal.trajectory.points[i].time_from_start = ros::Duration(1.0);
             i++;
             // Create second waypoints so each joint goes to 0.25
+            goal.trajectory.points[i].positions.resize(3);
             goal.trajectory.points[i].positions[0] = 0.25;
             goal.trajectory.points[i].positions[1] = 0.25;
             goal.trajectory.points[i].positions[2] = 0.25;
-            for(int j=0;j<goal.trajectory.points.size();i++)
+            goal.trajectory.points[i].velocities.resize(3);
+            goal.trajectory.points[i].accelerations.resize(3);
+            for(int j=0;j<3;++j)
             {
                 goal.trajectory.points[i].velocities[j] = 0.0;
                 goal.trajectory.points[i].accelerations[j] = 0.0;
@@ -98,6 +109,7 @@ int main(int argc, char** argv)
     leg.startTrajectory(goalTraj);
     while(!leg.getState().isDone() && ros::ok())
     {
+        ROS_INFO("Running leg trajectory");
     }
     return 0;
 }
